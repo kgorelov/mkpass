456ec5baadb11b561181fde591bd1f1ec22b144a
Kirill Gorelov <kgorelov@gmail.com>
2026-02-01 23:31:55 +0000

    Sorting char classes before generation to prevent bugs

--- src/mkpass.cpp
+++ src/mkpass.cpp
@@ -6,6 +6,7 @@
 #include "character_classes.h"
 #include <stdexcept>
 #include <vector>
+#include <algorithm>
 
 namespace {
 std::string CharClassToString(CharacterClass cc) {
@@ -21,34 +22,30 @@ std::string CharClassToString(CharacterClass cc) {
     }
     throw std::runtime_error("Unknown character class");
 }
+
+template <typename GeneratorType>
+std::string GenerateAndComposePassword(const Context& ctx) {
+    GeneratorType g(ctx.password, ctx.service);
+    if (ctx.char_classes.empty()) {
+        return ComposePassword(g, {LowercaseLetters, UppercaseLetters, Digits, Symbols}, ctx.length);
+    }
+    auto sorted_char_classes = ctx.char_classes;
+    std::sort(sorted_char_classes.begin(), sorted_char_classes.end());
+    std::vector<std::string> char_classes;
+    for (auto cc : sorted_char_classes) {
+        char_classes.push_back(CharClassToString(cc));
+    }
+    return ComposePassword(g, std::move(char_classes), ctx.length);
 }
+
+} // namespace
 
 std::string MkPass(const Context& ctx) {
     switch (ctx.algorithm) {
         case Algorithm::Argon2:
-        {
-            Generator<HKDF_Argon2> g(ctx.password, ctx.service);
-            if (ctx.char_classes.empty()) {
-                return ComposePassword(g, {UppercaseLetters, LowercaseLetters, Digits, Symbols}, ctx.length);
-            }
-            std::vector<std::string> char_classes;
-            for (auto cc : ctx.char_classes) {
-                char_classes.push_back(CharClassToString(cc));
-            }
-            return ComposePassword(g, std::move(char_classes), ctx.length);
-        }
+            return GenerateAndComposePassword<Generator<HKDF_Argon2>>(ctx);
         case Algorithm::SlowSha512:
-        {
-            Generator<HKDF_HMAC<SLOW_SHA512>> g(ctx.password, ctx.service);
-            if (ctx.char_classes.empty()) {
-                return ComposePassword(g, {UppercaseLetters, LowercaseLetters, Digits, Symbols}, ctx.length);
-            }
-            std::vector<std::string> char_classes;
-            for (auto cc : ctx.char_classes) {
-                char_classes.push_back(CharClassToString(cc));
-            }
-            return ComposePassword(g, std::move(char_classes), ctx.length);
-        }
+            return GenerateAndComposePassword<Generator<HKDF_HMAC<SLOW_SHA512>>>(ctx);
         case Algorithm::Old:
             return ComposeOldMkpass1Password(ctx.password, ctx.service, ctx.length);
     }
